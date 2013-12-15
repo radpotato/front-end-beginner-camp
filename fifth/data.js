69b10d29e1c80acc78f873f927bacea84e515de8
choeun <apes0123@gmail.com>
2013-12-15 13:52:14 +0900

    fifth

---
+++ fifth/data.js
@@ -0,0 +1,152 @@
+		var data = {
+			"movie" : [{
+				"title" : "111변호인",
+				"thumb" : "http://goo.gl/qwuLTI",
+				"openDate" : "12.18"
+			},
+			{
+				"title" : "222집으로 가는길",
+				"thumb" : "http://goo.gl/qwuLTI",
+				"openDate" : "12.18"
+			},
+			{
+				"title" : "333변호인",
+				"thumb" : "http://goo.gl/qwuLTI",
+				"openDate" : "12.18"
+			},
+			{
+				"title" : "444집으로 가는길",
+				"thumb" : "http://goo.gl/qwuLTI",
+				"openDate" : "12.18"
+			},
+			{
+				"title" : "55변호인",
+				"thumb" : "http://goo.gl/qwuLTI",
+				"openDate" : "12.18"
+			},
+			{
+				"title" : "66변호인",
+				"thumb" : "http://goo.gl/qwuLTI",
+				"openDate" : "12.18"
+			},
+			{
+				"title" : "77집으로 가는길",
+				"thumb" : "http://goo.gl/qwuLTI",
+				"openDate" : "12.18"
+			},
+			{
+				"title" : "변호인",
+				"thumb" : "http://goo.gl/qwuLTI",
+				"openDate" : "12.18"
+			},
+			{
+				"title" : "집으로 가는길",
+				"thumb" : "http://goo.gl/qwuLTI",
+				"openDate" : "12.18"
+			},
+			{
+				"title" : "101010변호인",
+				"thumb" : "http://goo.gl/qwuLTI",
+				"openDate" : "12.18"
+			},
+			{
+				"title" : "변호인",
+				"thumb" : "http://goo.gl/qwuLTI",
+				"openDate" : "12.18"
+			},
+			{
+				"title" : "집으로 가는길",
+				"thumb" : "http://goo.gl/qwuLTI",
+				"openDate" : "12.18"
+			},
+			{
+				"title" : "변호인",
+				"thumb" : "http://goo.gl/qwuLTI",
+				"openDate" : "12.18"
+			},
+			{
+				"title" : "14집으로 가는길",
+				"thumb" : "http://goo.gl/qwuLTI",
+				"openDate" : "12.18"
+			},
+			{
+				"title" : "15변호인",
+				"thumb" : "http://goo.gl/qwuLTI",
+				"openDate" : "12.18"
+			},
+			{
+				"title" : "변호인",
+				"thumb" : "http://goo.gl/qwuLTI",
+				"openDate" : "12.18"
+			},
+			{
+				"title" : "집으로 가는길",
+				"thumb" : "http://goo.gl/qwuLTI",
+				"openDate" : "12.18"
+			},
+			{
+				"title" : "변호인",
+				"thumb" : "http://goo.gl/qwuLTI",
+				"openDate" : "12.18"
+			},
+			{
+				"title" : "19집으로 가는길",
+				"thumb" : "http://goo.gl/qwuLTI",
+				"openDate" : "12.18"
+			},
+			{
+				"title" : "변호인",
+				"thumb" : "http://goo.gl/qwuLTI",
+				"openDate" : "12.18"
+			},
+			{
+				"title" : "변호인",
+				"thumb" : "http://goo.gl/qwuLTI",
+				"openDate" : "12.18"
+			},
+			{
+				"title" : "집으로 가는길",
+				"thumb" : "http://goo.gl/qwuLTI",
+				"openDate" : "12.18"
+			},
+			{
+				"title" : "변호인",
+				"thumb" : "http://goo.gl/qwuLTI",
+				"openDate" : "12.18"
+			},
+			{
+				"title" : "집으로 가는길",
+				"thumb" : "http://goo.gl/qwuLTI",
+				"openDate" : "12.18"
+			},
+			{
+				"title" : "변호인",
+				"thumb" : "http://goo.gl/qwuLTI",
+				"openDate" : "12.18"
+			},
+			{
+				"title" : "변호인",
+				"thumb" : "http://goo.gl/qwuLTI",
+				"openDate" : "12.18"
+			},
+			{
+				"title" : "집으로 가는길",
+				"thumb" : "http://goo.gl/qwuLTI",
+				"openDate" : "12.18"
+			},
+			{
+				"title" : "변호인",
+				"thumb" : "http://goo.gl/qwuLTI",
+				"openDate" : "12.18"
+			},
+			{
+				"title" : "집으로 가는길",
+				"thumb" : "http://goo.gl/qwuLTI",
+				"openDate" : "12.18"
+			},
+			{
+				"title" : "변호인",
+				"thumb" : "http://goo.gl/qwuLTI",
+				"openDate" : "12.18"
+			}]
+		}
---
+++ fifth/slide.js
@@ -0,0 +1,40 @@
+		var imageSlide = function(selector) {
+			return imageSlide.pbn.init(selector);	
+		}
+		imageSlide.pbn = imageSlide.prototype = {
+			init : function(selector) {
+				this.target = document.getElementById(selector);
+				return this;
+			},
+			createNode : function(option) {
+				var innerTxt = "<li><img width='105' src="+option.thumb+" alt="+option.title+">"+option.title+"<span>"+option.openDate+"</span></li>";
+				if(this.target) {
+					this.target.innerHTML += innerTxt;
+				} else {
+					this.innerHTML += innerTxt;
+				}
+				return this;
+			},
+			resetNode : function() {
+				this.target.innerHTML = "";
+				return this;
+			},
+			slide : function(index) {	
+				var length = 5,
+				startPoint = index * length,
+				destination = startPoint + length;
+				this.target.constructor.prototype.createNode = this.createNode;
+				for(i = startPoint; i < destination; i+=1) {
+					this.target.createNode(data.movie[i]);
+				}
+				return this;
+			},
+			on : function(eventer, func, bubble) {
+				this.target.addEventListener(eventer, func, bubble);
+				return this;
+			},
+			css : function(styleAttr) {
+				this.target.setAttribute("style", styleAttr);
+				return this;
+			}
+		}
